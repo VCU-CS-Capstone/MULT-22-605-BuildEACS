42828c2b5bb1dbbf97673481a0c182cdb8355fb8
Michael Blatt <59925198+blattmt@users.noreply.github.com>
2022-02-09 18:08:39 -0500

    Add files via upload
    Uploaded python 2.7 version of client code so that It can work with the existing Pi code

---
+++ ClientServer/cap_client2.py
@@ -0,0 +1,35 @@
+#Client side base program. 
+#Will connect to server side, send test data consisting
+#of ip address and member address as a string delimited by a '|'
+#Upon response from server: 
+# will check the response and either deny or allow access
+
+#Last update: 09FEB22
+from socket import *
+
+#Declaring ip and port number for socket
+serverName = '127.0.0.1'
+serverPort = 22222
+
+#Create socket object
+client = socket(AF_INET, SOCK_STREAM)
+
+#Connection request
+client.connect((serverName, serverPort))
+
+#Prompt for input ----------Make this the member ID
+#testData = raw_input()
+testData = '192.442.456.813|1235'
+
+#send date to server
+client.send(testData)
+
+#Receive server response and decode
+servResponse = client.recvfrom(2048)
+servResponse = servResponse[0].decode()
+
+#print data from server
+print (servResponse)
+
+#close socket
+client.close()
