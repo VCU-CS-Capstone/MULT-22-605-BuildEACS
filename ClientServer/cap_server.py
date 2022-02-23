1c8bf6b728b907a1563ede08d195335bf5d42d42
Michael Blatt <59925198+blattmt@users.noreply.github.com>
2022-02-23 16:57:04 -0500

    Add files via upload
    Threading implementation fixed.
    Continuing goals:
       -Formatting for log files
       -Attempt to get code to recognize updates to database without restarting

--- ClientServer/cap_server.py
+++ ClientServer/cap_server.py
@@ -1,63 +1,109 @@
 #Server side test program
 #Will await connection from client side
 #Once connection is made, will read in data sent from client,
-# split it into ip adress and member address based on delimiter '|'
-#Once split, will run query and return a true or false, based on response
-#from the query
+#data should consist of member id number. We can get ip address from the tcp packet
+#using that data we will run the query on the ip address to get the cert needed
+#and will then check to see if that user had that cert. 
+#if so, we send true back to client, false if not.
 
-#Last update: 02FEB22
-
+#Last update: 23FEB22
 import socket
+import datetime
+import threading
 from mysql.connector import (connection)
+
+#------can use this(loopback) for testing on same machine
+HOST = '127.0.0.1'
+#Get host/server ip address
+#HOST = socket.gethostbyname(socket.gethostname())
+#declare port number
+PORT = 12000
+DISCONNECT_MSG = '!DISCONNECT'
+HEADER = 64
+FORMAT = 'utf-8'
 
 #create database connection
 cnx = connection.MySQLConnection(user='root', password='CapstonePassword', host='127.0.0.1', database='sys')
-
+#create cursor object to allow running of query
 cursor = cnx.cursor()
 #the query used to check if a user is certified
 query = ("SELECT userID FROM CertUser WHERE certID = (SELECT certID FROM EqCert WHERE equipID = (SELECT equipID FROM Equipment WHERE wifi_Address = %s))")
-
-
-
-#declare host ip and port number
-HOST = '127.0.0.1'
-PORT = 22222
 
 #create socket object
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #assign ip and port to socket
 serv.bind((HOST,PORT))
-#begin listing for connections on socket
-serv.listen()
 
-#begin loop to continue listening on socket
-while True:
-    #read socket object and address from client
-    connection_socket, addr = serv.accept()
-    print('accepted')
+#Function to handle connections, get data from client, check against query
+#and return results
+def handle_client(conn, addr):
+    #Display which address of new connection
+    print(f'[NEW CONNECTION] {addr} connected.')
+    
+    #Loop condition variable
+    connected = True
+    while connected:
+        #Read member number from client
+        mID = conn.recv(32).decode(FORMAT)
+        
+        #Display member number sent from address
+        
+        print(f'[{addr}] Sent member ID: {mID}')
 
-    #Take in data from client
-    rData = connection_socket.recv(1024).decode()
+        #Run db query using address of machine sending request
+        cursor.execute(query, (addr,))
 
-    #Data should be in form of <ipv4>|<memberID>
-    #split based on delimiter
-    addr, mID = rData.split('|')
-    #print ipv4 and memberID
-    print('Machine address:', addr)
-    print('Member ID:', mID)
+        #based on return of the query,
+        #decline or allow based on member ID having req certification
+        response = 'True'
+        for userID in cursor:
+            #Display user ID
+            #print('{}'.format(userID))
+            #if mID is allowed list, send True and log...
+            if int(mID) in userID:
+                print('Verified User')
+                conn.send(response.encode(FORMAT))
+                #logFile = open(logPath, 'a')
+                #Write to file in sequence 'Granted/id/machine/date and time
+                #logFile.write('ACCESS GRANTED   ' + mID)....
+                #logFile.close()
+            #if not, send false and log
+            else:
+                response = 'False'
+                print('Unverified User')
+                conn.socket.send(response.encode(FORMAT))
+                #logFile = open(logPath, 'a')
+                #Write to file in sequence 'Denied/id/machine/date and time
+                #logFile.write('ACCESS GRANTED   ' + mID)....
+                #logFile.close()
+        connected = False
+    #close connection
+    conn.close()
+#Function to begin listening on network 
+def start():
+    #begin listening for connections on socket
+    serv.listen()
+    #print confirmation of socket listening and on what address
+    print(f'[LISTENING] Server is listening on {HOST}')
+    #print('[ENTER "quit" TO TERMINATE')
+    while True:
+        conn, addr = serv.accept()
+        addr = addr[0]
+        thread = threading.Thread(target=handle_client, args=(conn, addr))
+        thread.start()
+        print(f'[ACTIVE CONNECTIONS] {threading.active_count() - 1}')
 
-    cursor.execute(query, (addr,))
+##MAIN###########################
 
-    for userID in cursor:
-        print('{}'.format(userID))
-        if int(mID) in userID:
-            print('Verified User')
-            connection_socket.send('True'.encode())
-        else:
-            print('Unverified User')
-            connection_socket.send('False'.encode())
+#define date object
+currDate = datetime.date.today()
+#define filepath for log
+logPath = '/Users/blattmt/Desktop/python/school/' + str(currDate).split('-')[0] + '-' + str(currDate).split('-')[1] + '.txt'
 
-    cursor.close()
-    cnx.close()
-    #close connection to that client
-    connection_socket.close()
+###Run driver function
+start()
+
+#close cursor object
+cursor.close()
+#close database connection
+cnx.close()
