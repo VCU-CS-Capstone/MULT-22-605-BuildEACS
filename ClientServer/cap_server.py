78f1381d12ef3518f1f24beb2524f24c98ca2388
Michael Blatt <59925198+blattmt@users.noreply.github.com>
2022-03-02 15:08:08 -0500

    Add files via upload
    Discovered we were getting correct responses to send to the client but for the wrong reasons. Now checks for an empty list being returned first, then, if not empty, goes into normal checks to see if member is amongst list of authorized users. 
    
    Logging functionality has been finished and will create a new file when needed(Currently monthly) and append in a comma separated format of:
    "Granted/Denied, userID, machineIP, date-time"

--- ClientServer/cap_server.py
+++ ClientServer/cap_server.py
@@ -6,7 +6,7 @@
 #and will then check to see if that user had that cert. 
 #if so, we send true back to client, false if not.
 
-#Last update: 23FEB22
+#Last update: 02MAR22
 import socket
 import datetime
 import threading
@@ -17,15 +17,9 @@ HOST = '127.0.0.1'
 #Get host/server ip address
 #HOST = socket.gethostbyname(socket.gethostname())
 #declare port number
-PORT = 12000
-DISCONNECT_MSG = '!DISCONNECT'
+PORT = 12001
 HEADER = 64
 FORMAT = 'utf-8'
-
-#create database connection
-cnx = connection.MySQLConnection(user='root', password='CapstonePassword', host='127.0.0.1', database='sys')
-#create cursor object to allow running of query
-cursor = cnx.cursor()
 #the query used to check if a user is certified
 query = ("SELECT userID FROM CertUser WHERE certID = (SELECT certID FROM EqCert WHERE equipID = (SELECT equipID FROM Equipment WHERE wifi_Address = %s))")
 
@@ -33,13 +27,20 @@ query = ("SELECT userID FROM CertUser WHERE certID = (SELECT certID FROM EqCert 
 serv = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 #assign ip and port to socket
 serv.bind((HOST,PORT))
+#define date object
+currDate = datetime.date.today()
+# -------------------UPDATE to filepath desired on server system when installed
+logPath = '/Users/blattmt/Desktop/python/school/' + str(currDate).split('-')[0] + '-' + str(currDate).split('-')[1] + '.txt'
 
 #Function to handle connections, get data from client, check against query
 #and return results
 def handle_client(conn, addr):
     #Display which address of new connection
     print(f'[NEW CONNECTION] {addr} connected.')
-    
+    #Create database connection
+    cnx = connection.MySQLConnection(user='root', password='CapstonePassword', host='127.0.0.1', database='sys')
+    #Cursor object to let us run query
+    cursor = cnx.cursor()
     #Loop condition variable
     connected = True
     while connected:
@@ -56,27 +57,48 @@ def handle_client(conn, addr):
         #based on return of the query,
         #decline or allow based on member ID having req certification
         response = 'True'
-        for userID in cursor:
-            #Display user ID
-            #print('{}'.format(userID))
-            #if mID is allowed list, send True and log...
-            if int(mID) in userID:
-                print('Verified User')
-                conn.send(response.encode(FORMAT))
-                #logFile = open(logPath, 'a')
-                #Write to file in sequence 'Granted/id/machine/date and time
-                #logFile.write('ACCESS GRANTED   ' + mID)....
-                #logFile.close()
-            #if not, send false and log
-            else:
-                response = 'False'
-                print('Unverified User')
-                conn.socket.send(response.encode(FORMAT))
-                #logFile = open(logPath, 'a')
-                #Write to file in sequence 'Denied/id/machine/date and time
-                #logFile.write('ACCESS GRANTED   ' + mID)....
-                #logFile.close()
+
+        # print(cursor.fetchall())
+        check = cursor.fetchall()
+        # if check.rowcount == 0:
+        if not check:
+            response = 'False'
+            print('Unverified User')
+            print('\n'+logPath+'\n')
+            conn.send(response.encode(FORMAT))
+            logFile = open(logPath, 'a')
+            #Write to file in sequence 'Denied/id/machine/date and time
+            logFile.write('DENIED,' + mID + ',' + addr + ',' + str(datetime.datetime.now()).split('.')[0] +'\n')
+            logFile.close()
+        else:
+            for userID in check:
+                #Display user ID
+                #if mID is allowed and in list, send True and log...
+                print(userID)
+                if int(mID) in userID:
+                    print("Here is the mID " + mID + "\n")
+                    print('Verified User')
+                    conn.send(response.encode(FORMAT))
+                    logFile = open(logPath, 'a')
+                    #Write to file in sequence 'Granted/id/ip/date and time
+                    logFile.write('GRANTED,' + mID +' ,' + addr + ',' + str(datetime.datetime.now()).split('.')[0] + '\n')
+                    logFile.close()            
+                #if not, send false and log
+                else:
+                    #response = 'False'
+                    print('Unverified User')
+                    conn.send(response.encode(FORMAT))
+                    logFile = open(logPath, 'a')
+                    #Write to file in sequence 'Denied/id/machine/date and time
+                    logFile.write('DENIED,' + mID + ',' + addr + ',' + str(datetime.datetime.now()).split('.')[0] +'\n')
+                    logFile.close()
+        # connected = False
         connected = False
+
+    #close cursor object
+    cursor.close()
+    #close database connection
+    cnx.close()
     #close connection
     conn.close()
 #Function to begin listening on network 
@@ -85,8 +107,8 @@ def start():
     serv.listen()
     #print confirmation of socket listening and on what address
     print(f'[LISTENING] Server is listening on {HOST}')
-    #print('[ENTER "quit" TO TERMINATE')
-    while True:
+    runLoop = True
+    while runLoop :
         conn, addr = serv.accept()
         addr = addr[0]
         thread = threading.Thread(target=handle_client, args=(conn, addr))
@@ -94,16 +116,5 @@ def start():
         print(f'[ACTIVE CONNECTIONS] {threading.active_count() - 1}')
 
 ##MAIN###########################
-
-#define date object
-currDate = datetime.date.today()
-#define filepath for log
-logPath = '/Users/blattmt/Desktop/python/school/' + str(currDate).split('-')[0] + '-' + str(currDate).split('-')[1] + '.txt'
-
-###Run driver function
+#Run driver function
 start()
-
-#close cursor object
-cursor.close()
-#close database connection
-cnx.close()
